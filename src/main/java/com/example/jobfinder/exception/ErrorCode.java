a94d0d29157613e6c57c6e80199a7ff7e8b2bb47
letritrung <letritrung2605@gmail.com>
2025-07-04 21:07:31 +0700

    crud experience

--- src/main/java/com/example/jobfinder/exception/ErrorCode.java
+++ src/main/java/com/example/jobfinder/exception/ErrorCode.java
@@ -153,7 +153,10 @@ public enum ErrorCode {
     INVALID_ROLE(2011, "Role not permission"),
     EXPERIENCE_NOT_FOUND(2012," Experience not found"),
     SAVED_JOB_NOT_FOUND(2013, "You have not saved this job"),
-    TOKEN_EXPIRED(2014, "Token has expired");
+    TOKEN_EXPIRED(2014, "Token has expired"),
+    EXPERIENCE_EXISTED(2015, "EXPERIENCE_EXISTED");
+
+
 
 
     ErrorCode(int errorCode, String errorMessage) {
