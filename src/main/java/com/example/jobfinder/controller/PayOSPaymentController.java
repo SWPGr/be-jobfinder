d537ae1a8f728cbebac80a9239d004bfec721190
letritrung <letritrung2605@gmail.com>
2025-07-23 22:18:02 +0700

    fix empty payment history

--- src/main/java/com/example/jobfinder/controller/PayOSPaymentController.java
+++ src/main/java/com/example/jobfinder/controller/PayOSPaymentController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import vn.payos.type.CheckoutResponseData;
-import vn.payos.type.Webhook;
 
 import java.util.Collections;
 import java.util.Map;
