bec7d9ddf3272aac399e5ace28f3d9f70fd65ba8
phihung1902 <hungnp170043@fpt.edu.vn>
2025-07-25 22:08:28 +0700

    update

--- src/main/java/com/example/jobfinder/service/EmailService.java
+++ src/main/java/com/example/jobfinder/service/EmailService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     private final JavaMailSender mailSender;
 
     public EmailService(JavaMailSender mailSender) {
