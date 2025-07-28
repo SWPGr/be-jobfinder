0058eb63f1d4b5edc59f1d885428fa2c8d079847
letritrung <letritrung2605@gmail.com>
2025-07-28 12:07:25 +0700

    add status in candidates response

--- src/main/java/com/example/jobfinder/dto/job/CandidateDetailResponse.java
+++ src/main/java/com/example/jobfinder/dto/job/CandidateDetailResponse.java
@@ -3,6 +3,7 @@ package com.example.jobfinder.dto.job;
 
 import com.example.jobfinder.dto.user.JobSeekerResponse;
 import com.example.jobfinder.model.UserDetail;
+import com.example.jobfinder.model.enums.ApplicationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,5 +19,6 @@ public class CandidateDetailResponse {
     private String fullname;
     private String email;
     private String role;
+    private ApplicationStatus status;
     private JobSeekerResponse seekerDetail; // Thông tin chi tiết của ứng viên
 }
