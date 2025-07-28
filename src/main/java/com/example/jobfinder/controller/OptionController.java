35b5f3bbcd10ab2c0e44c42bf84a931abc8a53a0
phihung1902 <hungnp170043@fpt.edu.vn>
2025-07-28 14:25:39 +0700

    fix update

--- src/main/java/com/example/jobfinder/controller/OptionController.java
+++ src/main/java/com/example/jobfinder/controller/OptionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/options")
+@RequestMapping("/api/options")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class OptionController {
