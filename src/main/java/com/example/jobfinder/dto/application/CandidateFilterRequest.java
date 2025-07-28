91d62cbe82f21bdebe842fcf5194faa3a4740e17
letritrung <letritrung2605@gmail.com>
2025-07-28 22:40:12 +0700

    API: thay đổi trạng thái trang Overview, API: thêm phone cho profile employer

--- src/main/java/com/example/jobfinder/dto/application/CandidateFilterRequest.java
+++ src/main/java/com/example/jobfinder/dto/application/CandidateFilterRequest.java
@@ -12,11 +12,10 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CandidateFilterRequest {
-    // Các trường để lọc
     String fullName;
     String email;
     String location;
-    String experienceName; // Tên kinh nghiệm (ví dụ: "Entry Level", "Mid Level")
-    String educationName;  // Tên học vấn (ví dụ: "Bachelor's Degree", "Master's Degree")
-    Boolean isPremium; // Nếu bạn muốn lọc ứng viên premium (từ User Entity)
+    String experienceName;
+    String educationName;
+    Boolean isPremium;
 }
